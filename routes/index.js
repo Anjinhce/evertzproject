feb0919559fbdbf0824887a29a32bdd90a02bad5
anjinhce <anjaneya.hea@gmail.com>
2020-03-09 14:28:37 +0530

    index

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     getHomePage: (req, res) => {
-        let query = "SELECT * FROM employee ORDER BY id DESC"; // query database to get all the Employees
+        let query = "SELECT employee.EMP_ID,employee.PHOTO_PATH,employee.FIRTS_NAME,employee.MIDDLE_NAME,employee.LAST_NAME,designation.DESIGNATION,employee.DATE_OF_JOIN,employee.DATE_OF_BIRTH,employee.P_EMAIL,employee.P_PHONE,employee.W_EMAIL,employee.W_PHONE from employee,gender,designation WHERE employee.GENDER_ID=gender.ID and designation.ID=employee.DESIGNATION_ID";
 
 
         // execute query
